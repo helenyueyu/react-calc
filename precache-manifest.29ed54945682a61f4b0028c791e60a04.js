da7ba586e2d96dc0bf8ab2038af7dce16ec2e3b3
tokyosuite <helenyueyu@gmail.com>
2019-01-16 16:17:43 -0800

    Updates

---
+++ precache-manifest.29ed54945682a61f4b0028c791e60a04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "795a0190b36bbd49bee2",
+    "url": "/static/css/main.abf1ff26.chunk.css"
+  },
+  {
+    "revision": "795a0190b36bbd49bee2",
+    "url": "/static/js/main.795a0190.chunk.js"
+  },
+  {
+    "revision": "ac5061da54c65d4799c1",
+    "url": "/static/js/1.ac5061da.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "55af1024789b5e98e18d5f9502b88318",
+    "url": "/index.html"
+  }
+];
